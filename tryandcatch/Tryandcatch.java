7a73a53a63a43e9108b483620ee1bc6d70c2ccef
alice225k <najjembaalice225@gmail.com>
2015-10-15 14:09:40 -0700

    try and catch

---
+++ tryandcatch/Tryandcatch.java
@@ -0,0 +1,23 @@
+
+class tryandcatch{
+    public static void main(String args[]){
+        int num1,num2;
+                try{
+                    //try block to handle code that ma
+                    num1=2; 
+                    num2=62/num1;
+                    System.out.println("try block message");
+                            }
+                catch(ArithmeticException e)
+                          {
+                                //this block is to catch divid
+                                System.out.println("Error: done");
+                            }
+                            System.out.println("am out of try -catch");
+                            
+                }
+    }
+   
+
+
+
